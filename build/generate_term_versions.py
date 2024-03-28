155876f077cd8cb9cad4f1bf49b31442e079d307
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2024-03-28 17:17:29 -0500

    Fix problem with generate_term_versions

--- build/generate_term_versions.py
+++ build/generate_term_versions.py
@@ -164,12 +164,14 @@ for qrg_index,qrg_row in qrg_df.iterrows():
     for row_index,row in normative_doc_df.iterrows():
         if (qrg_row['recommended_term_iri'] == row['term_iri']) and (row['status'] == 'recommended'):
             found = True
-#            built_rows_df = built_rows_df.append(row)
-            built_rows_df = pd.concat([built_rows_df, row])
+            #built_rows_df = built_rows_df.append(row)
+            built_rows_df.loc[len(built_rows_df.index)] = row
             remaining_rows_df.drop(row['iri'], axis=0, inplace=True)
             break
     if not found:
-        print(qrg_row['recommended_term_iri'])
+        print('row not found:', qrg_row['recommended_term_iri'])
+
+print(built_rows_df)
 
 # Alphabetize remaining term versions
 #remaining_rows_df.sort_values(by='iri', inplace=True)
@@ -178,7 +180,7 @@ sorted_output = remaining_rows_df.iloc[remaining_rows_df.iri.str.lower().argsort
 # Concatenate ordered terms and remaining versions
 #normative_doc_df = built_rows_df.append(remaining_rows_df)
 #normative_doc_df = built_rows_df.append(sorted_output)
-normative_doc_df = pd.concat([normative_doc_df, sorted_output])
+normative_doc_df = pd.concat([built_rows_df, sorted_output])
 
 # Save the normative document DataFrame as a CSV
 normative_doc_df.to_csv('../vocabulary/term_versions.csv', index = False)
